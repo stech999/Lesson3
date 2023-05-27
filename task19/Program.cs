c0bec0e7d4e49980b7dfa703982a3e22b7b2d3eb
Красавчик <stech999@ya.ru>
2023-05-27 21:06:20 +0300

    два варианта рещения задачи

--- task19/Program.cs
+++ task19/Program.cs
@@ -6,44 +6,38 @@
 
 Console.Clear();
 
-Console.Write("Введите число для проверки в палиндроме: ");
-     bool palindrome = true;
-     int x = int.Parse(Console.ReadLine()!);
-     int c = x.ToString().Length - 1;
-     string b = x.ToString();
-     for (int i = 0; i < c; i++)
-          if (b[i] != b[c - i])
-          palindrome = false;
-          if (palindrome == true)
-          Console.Write("ДА");
-     else Console.Write("НЕТ");
-
-
-// так и не получилось напистаь через метод void
-
-// void tet (int m)
-// {
-//     //Console.Write("Введите число для проверки в палиндроме: ");
-//     //int m = int.Parse(Console.ReadLine()!);
-//     Console.Write(m);
-//
-//     bool palindrome = true;
-//     //Console.Write(m);
-//     int n = Int32.Parse(Console.ReadLine()!);
-    
-//     int c = n.ToString().Length - 1;
-//     string b = n.ToString();
-//     for (int i = 0; i < c; i++)
-//     if (b[i] != b[c - i])
-//     palindrome = false;
-//     if (palindrome == true)
-//     {
-//         Console.Write("ДА");
-//     }
-//     else Console.Write("НЕТ");
-//     //return n;
-// }
+// Первый метод
 
 // Console.Write("Введите число для проверки в палиндроме: ");
-// int res = int.Parse(Console.ReadLine()!);
-// tet(res);
+//      bool palindrome = true;
+//      int x = int.Parse(Console.ReadLine()!);
+//      int c = x.ToString().Length - 1;
+//      string b = x.ToString();
+//      for (int i = 0; i < c; i++)
+//           if (b[i] != b[c - i])
+//           palindrome = false;
+//           if (palindrome == true)
+//           Console.Write("ДА");
+//      else Console.Write("НЕТ");
+
+
+// Второй метод
+
+void tet (int m)
+{
+    bool palindrome = true;
+    int c = m.ToString().Length - 1;
+    string b = m.ToString();
+    for (int i = 0; i < c; i++)
+    if (b[i] != b[c - i])
+    palindrome = false;
+    if (palindrome == true)
+    {
+        Console.Write("ДА");
+    }
+    else Console.Write("НЕТ");
+}
+
+Console.Write("Введите число для проверки в палиндроме: ");
+int kek = int.Parse(Console.ReadLine()!);
+tet(kek);
