1e980db97705c24570c0327c2ed53a84d795b76d
Красавчик <stech999@ya.ru>
2023-05-27 23:23:57 +0300

    задание имеет 2 решения

--- task23/Program.cs
+++ task23/Program.cs
@@ -2,4 +2,35 @@
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+Console.Clear();
 
+// Первое решение
+
+// Console.Write("Введите цифру для расчета куба: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int[] num = new int[n];
+// int z = 0;
+// while (z < n)
+// {
+//     int v = z + 1;
+//     Console.WriteLine(v * v * v);
+//     z++;
+// }
+
+// Второе рещение
+
+void tet (int n)
+{
+    int[] num = new int[n];
+    int z = 0;
+    while (z < n)
+    {
+        int v = z + 1;
+        Console.WriteLine(v * v * v);
+        z++;
+    }
+}
+
+Console.Write("Введите цифру для расчета куба: ");
+int res = int.Parse(Console.ReadLine()!);
+tet(res);
