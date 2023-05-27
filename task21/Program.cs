353f42be885b3f6e37b41256a23ad8027c4dc5b9
Красавчик <stech999@ya.ru>
2023-05-27 22:13:50 +0300

    решение 21 задачи

--- task21/Program.cs
+++ task21/Program.cs
@@ -3,3 +3,17 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+// найти корень чисел
+Console.Clear();
+
+Console.WriteLine("введите координаты первой точки: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+double y1 = Convert.ToDouble(Console.ReadLine());
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите координаты второй точки: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+double y2 = Convert.ToDouble(Console.ReadLine());
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double result = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 -z1) * (z2 - z1));
+Console.WriteLine(Math.Round(result, 2));
