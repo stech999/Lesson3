98bac03edaca5d2b037e27bab4999ca29dc57ef1
Красавчик <stech999@ya.ru>
2023-05-28 20:30:54 +0300

    добавлено 2ое решение 21 задачи

--- task21/Program.cs
+++ task21/Program.cs
@@ -3,8 +3,28 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-// найти корень чисел
+// 1 Вариант решения задачи
 Console.Clear();
+
+// Console.WriteLine("введите координаты первой точки: ");
+// double x1 = Convert.ToDouble(Console.ReadLine());
+// double y1 = Convert.ToDouble(Console.ReadLine());
+// double z1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("введите координаты второй точки: ");
+// double x2 = Convert.ToDouble(Console.ReadLine());
+// double y2 = Convert.ToDouble(Console.ReadLine());
+// double z2 = Convert.ToDouble(Console.ReadLine());
+
+// double result = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 -z1) * (z2 - z1));
+// Console.WriteLine(Math.Round(result, 2));
+
+// 2 Вариант решения задачи
+
+void sq(double x1, double x2, double y1, double y2, double z1, double z2)
+{
+    double result = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+    Console.WriteLine(Math.Round(result, 2));
+}
 
 Console.WriteLine("введите координаты первой точки: ");
 double x1 = Convert.ToDouble(Console.ReadLine());
@@ -15,5 +35,4 @@ double x2 = Convert.ToDouble(Console.ReadLine());
 double y2 = Convert.ToDouble(Console.ReadLine());
 double z2 = Convert.ToDouble(Console.ReadLine());
 
-double result = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 -z1) * (z2 - z1));
-Console.WriteLine(Math.Round(result, 2));
+sq(x1, x2, y1, y2, z1, z2);
